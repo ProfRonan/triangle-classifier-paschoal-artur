fda9169e4ab5f3583543aad6d697aeb74af1dc9e
paschoal-artur <paschoal.artur@alu.ufc.br>
2023-04-05 12:08:18 -0300

    code correction2

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#Importing library
-import math
-
 #Receiving information from user
 side_a = int(input("Digite um valor para o lado 'a' do triângulo : \n"))
 side_b = int(input("Digite um valor para o lado 'b' do triângulo : \n"))
@@ -8,26 +5,23 @@ side_c = int(input("Digite um valor para o lado 'c' do triângulo : \n"))
 
 #Setting conditions to be a triangle
 cond_1 = int(side_a + side_b > side_c)
-cond_1 = True
 
 cond_2 = int(side_a + side_c > side_b)
-cond_2 = True
 
 cond_3 = int(side_b + side_c > side_a)
-cond_3 = True
 
-if cond_1 or cond_2 or cond_3 == False:
+if cond_1 == False or cond_2 == False or cond_3 == False:
     print("Não é um triângulo")
-    exit()
+else:
 #Type of triangle variables
-equilateral = int(side_a == side_b == side_c)
-isosceles = int(side_a == side_b or side_b == side_c or side_c == side_a)
-scalene = int(side_a != side_b != side_c)
+    equilateral = int(side_a == side_b == side_c)
+    isosceles = int(side_a == side_b or side_b == side_c or side_c == side_a)
+    scalene = int(side_a != side_b != side_c)
 
 #Telling what is the type of the triangle
-if equilateral :
-    print("Equilátero")
-elif isosceles :
-    print("Isósceles")
-elif scalene :
-    print("Escaleno")
+    if equilateral :
+        print("Equilátero")
+    elif isosceles :
+        print("Isósceles")
+    elif scalene :
+        print("Escaleno")
