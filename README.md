807382aca6e256123a6d2ad7623b3ce4f57ada07
Ronan Pardo Soares <ronan.soares@gmail.com>
2023-05-02 09:01:14 -0300

    Update testing with docker and reorganize files

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-24ddc0f5d75046c5622901739e7c5dd533143b0c8e959d652212380cedb1ea36.svg)](https://classroom.github.com/a/BdknfvaX)
 # Classificador de Triângulos
 
 O script principal de vocês deve estar no arquivo `main.py`.
@@ -30,17 +29,18 @@ Para testar automaticamente o programa **antes** de fazer um commit e enviar o s
    Para isso, basta executar o seguinte comando:
 
 ```bash
-python -m unittest
+python -m unittest discover -s test
 ```
 
 2. executar o arquivo `test_main.py` no terminal.
    Para isso, basta executar o seguinte comando:
 
 ```bash
-python test_main.py
+python test/test_main.py
 ```
 
 3. caso você esteja usando o [VSCode](https://code.visualstudio.com/), você pode abrir a paleta de comandos `CTRL+SHIFT+P` e digitar `Run All Tests`.
+
 4. no seu editor de código, você pode executar o arquivo `test_main.py` e verificar o resultado dos testes no terminal.
 
 ## 🤖 Observações Importantes 🤖
@@ -51,3 +51,63 @@ python test_main.py
 - **Antes de fazer um commit**, execute os testes usando um dos métodos acima para verificar se o seu programa está funcionando corretamente.
 - **Caso não consiga corrigir os erros**, entre em contato com o professor ou monitores para que eles possam te ajudar.
   Para isso você deve fazer um commit com o seu trabalho incompleto e abrir uma **issue** no repositório do exercício explicando o seu problema.
+
+## 👀 Curiosidades 👀
+
+O arquivo `requirements.txt` contém uma lista de dependências que o seu programa precisa para funcionar.
+
+No caso desse exercício, a única dependência é o módulo `unittest` que é usado para fazer os testes automáticos.
+E como o módulo `unittest` já vem instalado com o python, não é necessário instalar nada.
+
+> Quando precisarmos instalar dependências, o comando `pip` é usado para instalar pacotes do python.
+> Caso você precise instalar as dependências do seu programa, basta executar o seguinte comando:
+>
+> ```bash
+> pip install -r requirements.txt
+> ```
+
+Os arquivos `Dockerfile` contém as instruções para criar uma imagem do docker com o seu programa.
+Isso é útil para que eu possa executar o seu programa em um ambiente controlado e não ter problemas com dependências nem com possível códigos maliciosos na hora de rodar o programa.
+São usados dois arquivos `Dockerfile`, um para rodar o seu programa e outro para rodar os testes.
+
+Os arquivos dentro `.vscode` servem para configurar o ambiente de desenvolvimento no [VSCode](https://code.visualstudio.com/).
+E facilitar a execução dos testes e do programa.
+
+Os arquivos dentro da pasta `test` são usados para testar o seu programa.
+
+O arquivo `.gitignore` serve para dizer ao git quais arquivos ele deve ignorar.
+
+### :whale: Rodando o programa dentro do docker :whale:
+
+Para aqueles que gostariam de aprender mais sobre o [docker](https://www.docker.com/), aqui vai uma breve explicação de como usar o docker para rodar o seu programa e os testes.
+
+Primeiro, você precisa instalar o docker na sua máquina.
+Para isso, basta seguir as instruções do site do [docker](https://docs.docker.com/get-docker/).
+
+Depois de instalar o docker, você precisa criar uma imagem do docker com o seu programa.
+Para isso, você precisa criar um arquivo `Dockerfile` com as instruções para criar a imagem do docker.
+No caso desse exercício, o arquivo `Dockerfile` já está pronto.
+
+Para criar a imagem do docker com o seu programa, basta executar o seguinte comando:
+
+```bash
+docker build -t python-app .
+```
+
+Para rodar o seu programa dentro do docker, basta executar o seguinte comando:
+
+```bash
+docker run -it --rm python-app
+```
+
+Para criar a imagem do docker com os testes, basta executar o seguinte comando:
+
+```bash
+docker build -t python-app-test -f test/Dockerfile .
+```
+
+Para rodar os testes dentro do docker, basta executar o seguinte comando:
+
+```bash
+docker run -it --rm python-app-test
+```
--- test/test_main.py
+++ test/test_main.py
@@ -1,10 +1,11 @@
 """Test file for testing the main.py file"""
 
-import unittest
-from unittest.mock import patch
+import unittest # for creating the test case
+from unittest.mock import patch # for mocking the input
 import io # for capturing the output
 import sys # for restoring the stdout and removing the main module from the cache
 import importlib # for importing the main.py file
+from pathlib import Path # for getting the path of the main.py file
 
 class TestMain(unittest.TestCase):
     """Class for testing the main.py file"""
@@ -62,9 +63,9 @@ class TestMain(unittest.TestCase):
         self.assertNotIn("Isósceles", captured_output.getvalue().strip())
         self.assertNotIn("Escaleno", captured_output.getvalue().strip())
 
-    @patch("builtins.input", side_effect=["1", "2", "4"])
-    def test_1_2_4(self, _mock_input):
-        """Testa se o programa responde "Escaleno" quando o usuário digita 1, 2, 4"""
+    @patch("builtins.input", side_effect=["3", "4", "5"])
+    def test_3_4_5(self, _mock_input):
+        """Testa se o programa responde "Escaleno" quando o usuário digita 3, 4, 5"""
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
@@ -74,9 +75,9 @@ class TestMain(unittest.TestCase):
         self.assertNotIn("Equilátero", captured_output.getvalue().strip())
         self.assertNotIn("Isósceles", captured_output.getvalue().strip())
 
-    @patch("builtins.input", side_effect=["4", "2", "1"])
-    def test_4_2_1(self, _mock_input):
-        """Testa se o programa responde "Escaleno" quando o usuário digita 4, 2, 1"""
+    @patch("builtins.input", side_effect=["5", "4", "3"])
+    def test_5_4_3(self, _mock_input):
+        """Testa se o programa responde "Escaleno" quando o usuário digita 5, 4, 3"""
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
@@ -86,9 +87,9 @@ class TestMain(unittest.TestCase):
         self.assertNotIn("Equilátero", captured_output.getvalue().strip())
         self.assertNotIn("Isósceles", captured_output.getvalue().strip())
 
-    @patch("builtins.input", side_effect=["1", "1", "4"])
-    def test_1_1_4(self, _mock_input):
-        """Testa se o programa responde "Isósceles" quando o usuário digita 1, 1, 4"""
+    @patch("builtins.input", side_effect=["3", "3", "5"])
+    def test_3_3_5(self, _mock_input):
+        """Testa se o programa responde "Isósceles" quando o usuário digita 3, 3, 5"""
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
@@ -98,9 +99,9 @@ class TestMain(unittest.TestCase):
         self.assertNotIn("Equilátero", captured_output.getvalue().strip())
         self.assertNotIn("Escaleno", captured_output.getvalue().strip())
 
-    @patch("builtins.input", side_effect=["1", "4", "1"])
-    def test_1_4_1(self, _mock_input):
-        """Testa se o programa responde "Isósceles" quando o usuário digita 1, 4, 1"""
+    @patch("builtins.input", side_effect=["3", "5", "3"])
+    def test_3_5_3(self, _mock_input):
+        """Testa se o programa responde "Isósceles" quando o usuário digita 3, 5, 3"""
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
@@ -110,9 +111,9 @@ class TestMain(unittest.TestCase):
         self.assertNotIn("Equilátero", captured_output.getvalue().strip())
         self.assertNotIn("Escaleno", captured_output.getvalue().strip())
 
-    @patch("builtins.input", side_effect=["4", "1", "1"])
-    def test_4_1_1(self, _mock_input):
-        """Testa se o programa responde "Isósceles" quando o usuário digita 4, 1, 1"""
+    @patch("builtins.input", side_effect=["5", "3", "3"])
+    def test_5_3_3(self, _mock_input):
+        """Testa se o programa responde "Isósceles" quando o usuário digita 5, 3, 3"""
         captured_output = io.StringIO()
         sys.stdout = captured_output
         importlib.import_module("main")
@@ -123,4 +124,7 @@ class TestMain(unittest.TestCase):
         self.assertNotIn("Escaleno", captured_output.getvalue().strip())
 
 if __name__ == "__main__":
+    # add the parent directory to the path in order to run it from the run command in vscode
+    main_file_folder = Path(__file__).parents[1].as_posix() # pylint: disable=invalid-name
+    sys.path.insert(0, main_file_folder)
     unittest.main()
